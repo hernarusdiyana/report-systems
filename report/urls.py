5ccff149c7e0752d87be22bb0e1411ed49ed0a8e
hernarusdiyana <hernarusdiyana704@gmail.com>
2024-09-16 10:06:42 +0700

    update vercel package

--- report/urls.py
+++ report/urls.py
@@ -1,8 +1,7 @@
 from django.urls  import path
 from . import views
-from report.views import index
 
 urlpatterns = [
     # path('report/', views.report, name='report'),
-    path('', index),
+    path('', views.index, name='index'),
 ]
